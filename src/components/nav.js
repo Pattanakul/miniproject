47f3578092bfca3c1c7de0a2fb6e3b765d44b5b1
Pattanakul <sne456654@gmail.com>
2024-09-03 01:18:28 +0700

    ss

--- src/components/nav.js
+++ src/components/nav.js
@@ -59,7 +59,7 @@ const Navbar = () => {
     <nav className={`navbar navbar-expand-lg ${styles.navbarCustom}`}>
       <div className="container-fluid">
         <Link className="navbar-brand d-flex align-items-center" href="./">
-          <span className={styles.navbarBrandText}>Vasee</span>
+          <span className={styles.navbarBrandText}>My Gu</span>
         </Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon" />
@@ -70,13 +70,13 @@ const Navbar = () => {
               <Link className={`nav-link ${styles.navLink}`} href="./">Dashboard</Link>
             </li>
             <li className="nav-item">
-              <Link className={`nav-link ${styles.navLink}`} href="/History">Historys</Link>
+              <Link className={`nav-link ${styles.navLink}`} href="/History">History</Link>
             </li>
           </ul>
           <form className="d-flex align-items-center">
-            <button type="button" className={`btn ${styles.btnCustom} me-2`} onClick={() => updateLEDStatus('RGB_ON', setLEDStatus)}>เปิดไฟ</button>
-            <button type="button" className={`btn ${styles.btnCustom} me-2`} onClick={() => updateLEDStatus('BUZZER_ON', setLEDStatus)}>เปิดเสียง</button>
-            <button type="button" className={`btn ${styles.btnDanger}`} onClick={() => updateLEDStatus('OFF', setLEDStatus)}>ปิดทั้งหมด</button>
+            <button type="button" className={`btn ${styles.btnCustom} me-2`} onClick={() => updateLEDStatus('RGB_ON', setLEDStatus)}>Open System</button>
+            <button type="button" className={`btn ${styles.btnCustom} me-2`} onClick={() => updateLEDStatus('BUZZER_ON', setLEDStatus)}>Buzzer</button>
+            <button type="button" className={`btn ${styles.btnDanger}`} onClick={() => updateLEDStatus('OFF', setLEDStatus)}>Off</button>
             <span className={`ms-3 ${ledStatus ? styles.statusOn : styles.statusOff}`}>
               {ledStatus ? 'LED is ON' : 'LED is OFF'}
             </span>
